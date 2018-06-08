d73b423853ec618a2ab4c7f815becd904257402c
baochangfan <554205018@qq.com>
2018-06-08 17:40:47 +0800

    更新

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,19 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { IonicPage } from "ionic-angular";
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
-
+//import { HomePage } from "../home/home";
+@IonicPage()
 @Component({
-  templateUrl: 'tabs.html'
+  templateUrl: "tabs.html"
 })
 export class TabsPage {
-
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
-
-  constructor() {
-
-  }
+  tab1Root = "HomePage";
+  tab2Root = "MinePage";
+  constructor() {}
 }
